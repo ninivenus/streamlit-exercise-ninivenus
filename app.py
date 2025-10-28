ab4cd45ca0c6f7ddc5f7ee4a3aa1f3b29cd2204d
nini <sunc88@mcmaster.ca>
2025-10-28 14:09:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Medals Visualization", layout="wide")
 st.title("Medals Visualization")
 
 #Dropdown menu
-medal=st.selectbox("Medal type",["gold",'silver','bronze'])
+medal=st.selectbox("Medal type",['gold','silver','bronze'])
 
 #checkboxes
 show_bar=st.checkbox("Show Bar Chart",value=True)
